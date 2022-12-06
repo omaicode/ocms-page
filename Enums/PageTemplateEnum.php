906fb91e6bf270159a11d078119ba0348ced1292
Kiều Trọng Thiện <kieutrongthien@gmail.com>
2022-12-06 10:53:17 +0700

    Up

--- Enums/PageTemplateEnum.php
+++ Enums/PageTemplateEnum.php
@@ -8,10 +8,12 @@ use Omaicode\Enum\Enum;
  * @method static static DEFAULT()
  * @method static static HOME()
  * @method static static BLANK()
+ * @method static static BLOG()
  */
 final class PageTemplateEnum extends Enum
 {
     const DEFAULT =   0;
     const HOME    =   1;
     const BLANK   =   2;
+    const BLOG   =   3;
 }
